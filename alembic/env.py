3b7cc55fbdc16c06b17c394107b93afe245317af
Reza <rmwhdy8@gmail.com>
2026-07-19 18:13:37 +0330

    Enhance Alembic environment setup by importing models and adjusting sys.path

--- alembic/env.py
+++ alembic/env.py
@@ -1,9 +1,16 @@
+from app.database import Base
+import app.models
+import sys
+from pathlib import Path
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-from app.database import Base
 from alembic import context
+print(Base.metadata.tables.keys())
+# Add the parent directory to the path so imports work
+sys.path.insert(0, str(Path(__file__).parent.parent))
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
